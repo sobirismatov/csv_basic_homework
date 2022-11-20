ecf90466d169be37ae2cd991679de5c5881802c2
sobirismatov <name@uzcoder96@gmail.com>
2022-11-20 07:57:24 +0500

    ustunlar sonini topish

--- get_column_names.py
+++ get_column_names.py
@@ -7,6 +7,11 @@ def get_column_names(data):
     Returns:
         column_names: list of column names
     """
-    return
+    data=data.split("\n")
+    data=data[0].split(",")
+    return data
+f=open("data.csv")
+data=f.read()
+print(get_column_names(data))
     
 # Read the csv file
